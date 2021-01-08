6a9287445c0046f48076a8156f8047be63446da6
marios <mariosge90@gmail.com>
2021-01-08 23:30:49 +0200

    fetch results from opaps api

--- opapclient.py
+++ opapclient.py
@@ -1,6 +1,24 @@
 import opap
+import requests, json
 
-url = "https://api.opap.gr/"
+url = "https://api.opap.gr"
 
 def get_opap_last_draw():
     return opap.get_last_draw("joker")
+
+def get_last_joker_draw():
+    gameid = opap.get_draw_id("joker")
+    surl = url + "/draws/v3.0/" + gameid + "/last-result-and-active"
+
+    resp = requests.get(surl)
+    j = resp.text
+    jdata = json.loads(j)
+
+    response_model = {
+        "winningNumbers" : list(jdata["last"]["winningNumbers"]["list"]),
+        "bonusNumber"    : int(jdata["last"]["winningNumbers"]["bonus"][0]),
+        "drawTime"       : int(jdata["last"]["drawTime"]),
+        "drawId"         : int(jdata["last"]["drawId"])
+    }
+
+    return response_model
